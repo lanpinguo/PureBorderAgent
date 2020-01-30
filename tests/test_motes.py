f388eb1da4405b33496c94e7d0aee8d2e2bf8325
lanpinguo <lanpinguo1985@126.com>
2020-01-30 10:50:45 +0000

    optimize cmd parse

--- tests/test_motes.py
+++ tests/test_motes.py
@@ -12,6 +12,10 @@ from coap import coapObjectSecurity   as oscoap
 
 #import logging_setup
 
+motes_ip = { 'fish'      :'fd00::0212:4b00:1940:c0e3',
+			'front_light':'fd00::212:4b00:18f1:d9d2',
+			'back_light' : 'fd00::0212:4b00:1940:c17a'}
+
 
 def set_actuator(c,target,payload):
 	try:
@@ -40,11 +44,50 @@ def get_sensor(c,target):
 		print("Exception")
 		print(err)
 
+def cmd_parser(coap,args):
+	if len(args) == 0:
+		return 0
+	cmd = args[0]
+	if cmd == 'hcho':
+		mote = motes_ip['front_light']
+		get_sensor(coap,mote)
+	elif cmd == 'fish':
+		#fish jar control
+		mote = motes_ip['fish']
+		if len(args) < 2:
+			print('too short parameter')
+			return 1
+
+		mask = 0
+		if len(args) == 3:
+			if args[1] == 'all':
+				mask = ((1<<0)|(1<<1)|(1<<2)|(1<<3))
+			else:
+				try :
+					index = int(args[1])
+				except Exception as err:
+					print(err)
+					return 2
+				if index >= 4 :
+					print("Wrong Mask Parameter")
+					return 3
+				mask = (1<<index)
+
+		state = args[2]
+		if state == 'on':
+			payload = b"&state=%lx&mask=%lx" % (mask,mask)
+		elif state == 'off':
+			payload = b"&state=%lx&mask=%lx" % ((~mask) & 0xFFFFFFFF,mask)
+		else:
+			print('Wrong State Parameter')
+			return 4
+		print(payload)
+		set_actuator(coap,mote,payload)
+	elif cmd == 'exit':
+		return -1
+	return 0
+
 if __name__ == '__main__':
-	#SERVER_IP = 'fd00::212:4b00:1005:fdf1'
-	SERVER_IP = 'fd00::212:4b00:18f1:d9d2'
-	#SERVER_IP = 'fd00::0212:4b00:1940:c0e3'
-	print(SERVER_IP)
 	# open
 	c = coap.coap(udpPort=5683)
 
@@ -59,47 +102,10 @@ if __name__ == '__main__':
 	while True:
 		raw_in = input('mote#')
 		args = raw_in.split()
-		if len(args) == 0:
-			continue
-		cmd = args[0]
-		if cmd == 'hcho':
-			SERVER_IP = 'fd00::212:4b00:18f1:d9d2'
-			get_sensor(c,SERVER_IP)
-		elif cmd == 'fish':
-			#fish jar control
-			SERVER_IP = 'fd00::0212:4b00:1940:c0e3'
-			if len(args) < 2:
-				print('too short parameter')
-				continue
-
-			mask = 0
-			if len(args) == 3:
-				if args[1] == 'all':
-					mask = ((1<<0)|(1<<1)|(1<<2)|(1<<3))
-				else:
-					try :
-						index = int(args[1])
-					except Exception as err:
-						print(err)
-						continue
-					if index >= 4 :
-						print("Wrong Mask Parameter")
-						continue
-					mask = (1<<index)
-
-			state = args[2]
-			if state == 'on':
-				payload = b"&state=%lx&mask=%lx" % (mask,mask)
-			elif state == 'off':
-				payload = b"&state=%lx&mask=%lx" % ((~mask) & 0xFFFFFFFF,mask)
-			else:
-				print('Wrong State Parameter')
-				continue
-			print(payload)
-			set_actuator(c,SERVER_IP,payload)
-		elif cmd == 'exit':
+		result = cmd_parser(c,args)
+		print("cmd exec result: %d" % result)
+		if result < 0:
 			break
-
 	# close
 	c.close()
 

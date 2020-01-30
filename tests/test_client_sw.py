a5e6c69fea7ba72b88c559c49f218e109b0bf653
lanpinguo <lanpinguo1985@126.com>
2020-01-30 10:30:11 +0000

    fix bugs in sw test

--- tests/test_client_sw.py
+++ tests/test_client_sw.py
@@ -57,8 +57,10 @@ if __name__ == '__main__':
 
 
 	while True:
-		raw_in = input('#')
-		args = raw_in.split(' ')
+		raw_in = input('mote#')
+		args = raw_in.split()
+		if len(args) == 0:
+			continue
 		cmd = args[0]
 		if cmd == 'hcho':
 			SERVER_IP = 'fd00::212:4b00:18f1:d9d2'
@@ -66,17 +68,34 @@ if __name__ == '__main__':
 		elif cmd == 'fish':
 			#fish jar control
 			SERVER_IP = 'fd00::0212:4b00:1940:c0e3'
-			if len(args) != 2:
-				print('Wrong Parameter')
+			if len(args) < 2:
+				print('too short parameter')
 				continue
-			state = args[1]
+
+			mask = 0
+			if len(args) == 3:
+				if args[1] == 'all':
+					mask = ((1<<0)|(1<<1)|(1<<2)|(1<<3))
+				else:
+					try :
+						index = int(args[1])
+					except Exception as err:
+						print(err)
+						continue
+					if index >= 4 :
+						print("Wrong Mask Parameter")
+						continue
+					mask = (1<<index)
+
+			state = args[2]
 			if state == 'on':
-				payload = b"&state=%lx&mask=%lx" % (((1<<0)|(1<<2)|(1<<3)),((1<<0)|(1<<1)|(1<<2)|(1<<3)))
+				payload = b"&state=%lx&mask=%lx" % (mask,mask)
 			elif state == 'off':
-				payload = b"&state=%lx&mask=%lx" % (0,((1<<0)|(1<<1)|(1<<2)|(1<<3)))
+				payload = b"&state=%lx&mask=%lx" % ((~mask) & 0xFFFFFFFF,mask)
 			else:
-				print('Wrong Parameter')
+				print('Wrong State Parameter')
 				continue
+			print(payload)
 			set_actuator(c,SERVER_IP,payload)
 		elif cmd == 'exit':
 			break
